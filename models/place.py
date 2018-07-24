13dc8b4321e7376d981e0e88fe19585127162105
Derek <dhklangs@gmail.com>
2018-07-24 00:02:53 +0000

    Remove defaults on float columns

--- models/place.py
+++ models/place.py
@@ -25,8 +25,8 @@ class Place(BaseModel, Base):
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
         price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True, default=0.0)
-        longitude = Column(Float, nullable=True, default=0.0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
         amenity_ids = []
         user = relationship("User")
         cities = relationship("City")
