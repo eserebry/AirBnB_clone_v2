c9ca62558e64bc5ee9493af0797371300ea83da3
Derek <dhklangs@gmail.com>
2018-07-26 04:57:43 +0000

    Refactor __init__ code for base_model class

--- models/base_model.py
+++ models/base_model.py
@@ -25,19 +25,26 @@ class BaseModel:
         '''
             Initialize public instance attributes.
         '''
-        for key, val in kwargs.items():
-                if "__class__" not in key:
-                    setattr(self, key, val)
-
-        if id not in kwargs:
+        if (len(kwargs) == 0):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+
         else:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            try:
+                kwargs["created_at"] = datetime.strptime(
+                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                kwargs["updated_at"] = datetime.strptime(
+                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+
+            except KeyError:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def __str__(self):
         '''
