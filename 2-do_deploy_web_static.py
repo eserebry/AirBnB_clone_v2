8f2661af6bdda0944722f65bc0828a0c8d2b47e7
Derek <dhklangs@gmail.com>
2018-07-30 20:31:51 +0000

    Fix spacing issues

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,14 +15,14 @@ def do_deploy(archive_path):
     filename_wo_ext = archive_path[9:34]
     filename_w_ext = archive_path[9:]
     input_path = "/data/web_static/releases/{}/".format(filename_wo_ext)
+
     try:
         put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(input_path))
         run("tar -zxvf /tmp/{} -C {}".format(filename_w_ext, input_path))
         run("sudo rm /tmp/{}".format(filename_w_ext))
         run("sudo rm /data/web_static/current")
-        run("sudo ln - sf / data / web_static / releases / {} /
-            /data / web_static / current".format(filename_wo_ext))
+        run("sudo ln -sf {} /data/web_static/current".format(input_path))
         return True
 
     except BaseException:
