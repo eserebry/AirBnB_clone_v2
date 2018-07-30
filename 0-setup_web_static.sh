b86ef2f6655ff4d9b89d7369c262035cf8e616da
Derek <dhklangs@gmail.com>
2018-07-30 17:38:48 +0000

    Add root user privileges and restart nginx service

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,8 +2,8 @@
 # Sets up the web servers for the deployment of web_static
 
 # Install NGINX
-sudo apt-get update
-sudo apt-get nginx
+sudo apt-get -y update
+sudo apt-get -y nginx
 
 # Create folders
 mkdir /data/web_static/releases/test/
@@ -14,11 +14,13 @@ touch /data/web_static/releases/test/index.html
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
 
 # Create symbolic link
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Ownership of /data/ folder to user and group
-sudo chown -RL ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 # Configure Nginx to serve content
 content="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
 sudo sed -i "37i\ $content" /etc/nginx/sites-enabled/default
+sudo service nginx reload
+sudo service nginx restart
