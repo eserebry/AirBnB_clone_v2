0ea3bb563ff1458d350adb166bb245343ee76f7d
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-07-31 06:26:54 +0000

    initial commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+from fabric.api import *
+from datetime import datetime
+import os
+
+def do_deploy(archive_path):
+    """
+    distributes an archive to web servers
+    """
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+from fabric.api import *
+from datetime import datetime
+import os
+
+def deploy():
+    """
+    creates and distributes an archive to your web servers,
+    using the function deploy
+    """
