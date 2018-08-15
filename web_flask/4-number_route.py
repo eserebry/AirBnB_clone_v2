9728e10584e0d6744b896f476d1f26bdc8d21641
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-08-15 05:38:10 +0000

    first commit

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# starts a Flask web application
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_flask():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def flask():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    return 'C %s' % text.replace("_", " ")
+
+
+@app.route('/python', strict_slashes=False)
+def python_is():
+    return 'Python is cool'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_magic(text='is_cool'):
+    return 'Python %s' % text.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def int_only(n):
+    return '%d is a number' % n
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
