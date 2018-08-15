3cc1def65e49f19155f8d108c0e5c63710761084
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-08-15 02:40:16 +0000

    syntax changed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,9 +4,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    @app.route('/', strict_slashes=False)
-    def hello_flask():
-        return 'Hello HBNB!'
 
+@app.route('/', strict_slashes=False)
+def hello_flask():
+    return 'Hello HBNB!'
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
