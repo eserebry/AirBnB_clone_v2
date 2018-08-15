7d28d3bd5be0a0aca9d2d47d79a13077755b8f90
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-08-15 03:43:04 +0000

    first commit

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_flask():
     return 'Hello HBNB!'
+@app.route('/hbnb', strict_slashes=False)
+def flask():
+    return 'HBNB'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
