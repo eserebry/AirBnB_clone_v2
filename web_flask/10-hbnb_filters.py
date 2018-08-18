e9bb9ef02ffd0d80c20b67d1c306a856fc6c8a66
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-08-18 03:11:40 +0000

    first commit

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-# starts a Flask web application
+""" starts a Flask web application """
 
 from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
 app = Flask(__name__)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 
 @app.route('/', strict_slashes=False)
@@ -47,6 +52,49 @@ def odd_or_even(n):
     else:
         num = 'odd'
     return render_template('6-number_odd_or_even.html', n=n, num=num)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states():
+    states = storage.all(State)
+    print(states)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def by_states():
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.route('/states/', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id=None):
+    states = storage.all(State)
+    title = "States"
+    if id is not None:
+        for items in states.values():
+            if id == items.id:
+                states = items
+                title = "State: {}".format(items.name)
+                break
+            else:
+                title = "Not found!"
+    return render_template('9-states.html', states=states, id=id, title=title)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb():
+    states = storage.all(State)
+    amenity = storage.all(Amenity)
+    return render_template('10-hbnb_filters.html',
+                           states=states, amenity=amenity)
+
+
+@app.teardown_appcontext
+def teardown(exeption):
+    storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
