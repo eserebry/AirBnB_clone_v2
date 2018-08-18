bc06c4b8aafbcfdf2dc0fa095962a517048a542b
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-08-18 00:00:00 +0000

    first commit

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-# starts a Flask web application
+""" starts a Flask web application """
 
 from flask import Flask, render_template
+from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
@@ -47,6 +49,25 @@ def odd_or_even(n):
     else:
         num = 'odd'
     return render_template('6-number_odd_or_even.html', n=n, num=num)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states():
+    states = storage.all(State)
+    print(states)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def by_states():
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(exeption):
+    storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
